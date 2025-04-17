d82df40012fafae55fdf6ef9546fb389160405a2
Juan Camilo <juanmororo@unisabana.edu.co>
2025-04-16 23:51:15 -0500

    Update pedidos.js

--- JS/pedidos.js
+++ JS/pedidos.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("📦 Enviando datos:", datos);
 
         try {
-            const respuesta = await fetch("https://script.google.com/macros/s/AKfycbwYvN_uXb10TWfvMn7hv73U0iHHE-SExkqkdAw18hxtt2ErU6JE3q-W1eijQowR-A4w/exec", {
+            const respuesta = await fetch("https://script.google.com/macros/s/AKfycbyxLnLgI7Po_5OwrtsW9BrTUcArzi2froTJZKEnYBuLPTXQX-5m1bLdtgiH-VeqGrDu/exec", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(datos)
