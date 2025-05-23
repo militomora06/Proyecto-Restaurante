ab5f99a425fb0133c2b16dcd614c88e543350c73
AnaSofia64 <anarofe@unisabana.edu.co>
2025-05-23 16:07:56 -0500

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -190,3 +190,10 @@ if (window.location.pathname.includes("Pedidos.html")) {
         }
     }
 }
+
+fetch('http://localhost:8000/api/productos/')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data); // Renderiza productos
+  });
+
